3b354162613b89651a9b108ea82bcc9a8abb9f24
tomochang <the.yellow.tail@gmail.com>
2012-02-27 13:55:58 +0900

    Delete the useless variable.

--- Factorization.java
+++ Factorization.java
@@ -1,6 +1,5 @@
 class Factorization {
   public static void main(String args[]){
-    String s1 = "test";
     int i1 = Integer.parseInt(args[0]);
     if (i1 >= 0 ){
       for( int i=1; i<=i1 ; i= i + 1 ){
