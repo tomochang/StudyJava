1c1007a9ba1632b3b3d4399bd2fb96dc54bc4276
tomochang <the.yellow.tail@gmail.com>
2012-02-28 19:17:03 +0900

    コンストラクターをCircleクラスに追加した

--- Circle.java
+++ Circle.java
@@ -2,14 +2,21 @@ class Circle {
 	double x;
 	double y;
 	double radius;
+
+	Circle() {
+	}
+
+	Circle(double ax, double ay, double aradius){
+		x = ax;
+		y = ay;
+		radius = aradius;
+	}
 }
 
 class CircleExample {
 	public static void main(String args[]){
-		Circle c = new Circle();
-		c.x=12.1;
-		c.y=24.5;
-		c.radius=4;
+		Circle c = new Circle( 17.5,16.4,11.3 );
+
 		System.out.println(c.x);
 		System.out.println(c.y);
 		System.out.println(c.radius);
