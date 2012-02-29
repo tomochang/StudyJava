ab261e9d37e036b4d6890d1418f96055ed2662b8
tomochang <the.yellow.tail@gmail.com>
2012-02-29 14:50:09 +0900

    異動先の座標を定義する、move()というインスタンスメソッドを追加

--- Point3D.java
+++ Point3D.java
@@ -9,6 +9,12 @@ class Point3D{
 		this.z = z;
 	}
 
+	void move(double x,double y, double z){
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+
 }
 
 class Point3DConstructor {
@@ -16,6 +22,11 @@ class Point3DConstructor {
 
 		Point3D p = new Point3D(1.1,3.4,-4.7 );
 
+		System.out.println(p.x);
+		System.out.println(p.y);
+		System.out.println(p.z);
+
+		p.move(5, 5, 5);
 		System.out.println(p.x);
 		System.out.println(p.y);
 		System.out.println(p.z);
