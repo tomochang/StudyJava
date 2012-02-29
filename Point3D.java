061e04e577ceaa32ac002b5c23aa7a8295e92026
tomochang <the.yellow.tail@gmail.com>
2012-02-29 09:26:53 +0900

    コンストラクタをthis.を使うように修正

--- Point3D.java
+++ Point3D.java
@@ -3,10 +3,10 @@ class Point3D{
 	double y;
 	double z;
 
-	Point3D(double ax, double ay, double az){
-		x = ax;
-		y = ay;
-		z = az;
+	Point3D(double x, double y, double z){
+		this.x = x;
+		this.y = y;
+		this.z = z;
 	}
 
 }
