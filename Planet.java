c55aa90b581a2716a9cb15ec1a2dd4be075ef613
tomochang <the.yellow.tail@gmail.com>
2012-03-02 10:55:15 +0900

    惑星の名前を表示するプログラム

---
+++ Planet.java
@@ -0,0 +1,28 @@
+class Planet {
+	static String PlanetName[] = {"earth","moon","jupiter","mars"};
+	static int m = PlanetName.length;
+
+}
+
+class SolarSystem {
+
+	SolarSystem(){
+	}
+
+	void display(){
+		int i;
+		for(i = 0; i < Planet.m ; i++){
+			System.out.println(Planet.PlanetName[i]);
+		}
+	}
+}
+
+class callSolarSystem{
+
+	public static void main(String args[]){
+		SolarSystem test1 = new SolarSystem();
+		test1.display();
+	}
+
+
+}
