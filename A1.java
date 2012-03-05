ff6e583a08da5535787f31d49cc1e5ea64e2dba2
tomochang <the.yellow.tail@gmail.com>
2012-03-05 14:09:46 +0900

    オーバーライドのサンプル写経

---
+++ A1.java
@@ -0,0 +1,27 @@
+class A1{
+	void hello(){
+		System.out.println("Hello from A1");
+	}
+}
+
+class B1 extends A1 {
+	void hello(){
+		System.out.println("Hello from B1");
+	}
+}
+
+class C1 extends B1 {
+	void hello(){
+		System.out.println("Hello from C1");
+	}
+}
+
+class MethodOverriding1 {
+	public static void main(String args[]){
+		C1 obj_c = new C1();
+		B1 obj_b = new B1();
+
+		obj_b.hello();
+		obj_c.hello();
+	}
+}
