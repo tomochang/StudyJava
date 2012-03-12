8dfedec01e39fc57a068827f3c777840c98d2049
tomochang <the.yellow.tail@gmail.com>
2012-03-12 15:35:47 +0900

    protectedを追加

--- InheritanceAndConstructors1.java
+++ InheritanceAndConstructors1.java
@@ -14,20 +14,60 @@ class T1 extends S1{
 	}
 }
 
-class U1 extends T1{
+abstract class U1 extends T1{
 	int u1;
 	U1() {
 		System.out.println("U1 Constructor");
 		u1 = 3;
 	}
+
+	public final void display() {
+		predisplay();
+		System.out.println("test");
+		sufdisplay();
+	}
+
+	abstract protected void sufdisplay();
+
+	protected void predisplay() {
+	}
 }
 
 class InheritanceAndConstructors1 {
 	public static void main(String args[]){
-		U1 u1 =  new U1();
-		System.out.println(u1.s1);
-		System.out.println(u1.t1);
-		System.out.println(u1.u1);
 
+		U1 u1;
+		int i = 2;
+	if (i == 1){
+		u1 =  new U1(){
+
+			@Override
+			protected void sufdisplay() {
+				// TODO 自動生成されたメソッド・スタブ
+
+			}
+
+		};
+	} else {
+		u1 =  new U1(){
+			@Override
+			protected void predisplay() {
+				System.out.println("pre2");
+			}
+
+			@Override
+			protected void sufdisplay() {
+				// TODO 自動生成されたメソッド・スタブ
+
+			}
+
+
+		};
+	}
+//		System.out.println(u1.s1);
+//		System.out.println(u1.t1);
+//		System.out.println(u1.u1);
+
+		u1.display();
 	}
 }
