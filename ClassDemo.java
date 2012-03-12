20c9bcefee7d9a3330db999d641a6db18d9a5053
tomochang <the.yellow.tail@gmail.com>
2012-03-12 20:20:56 +0900

    ObjectクラスとClassクラスのメソッドの使い方を写経

---
+++ ClassDemo.java
@@ -0,0 +1,8 @@
+class ClassDemo{
+	public static void main(String args[]) {
+		Integer obj = new Integer(8);
+		Class cls = obj.getClass();
+		System.out.println(cls);
+		System.out.println(obj);
+	}
+}
